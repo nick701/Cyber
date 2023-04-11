0cad8ad952c540fce40acc52e29113972eb66e5b
Nikolaos Tiliakos <Nikolaos.Tiliakos@loblaw.ca>
2023-04-11 16:39:47 -0400

    Initial commit

---
+++ test3.py
@@ -0,0 +1,8 @@
+def exp(num1, num2):
+    return num1 * num2
+
+# Random quote from People rarely succeed unless they have fun in what they are doing.: Bill Gates
+# Random quote: A man is a success if he gets up in the morning and gets to bed at night, and in: Anon.
+# Random quote:
+ There are no secrets to success. It is the result of preparation, hard work, and: 
+Aisha Tyler
