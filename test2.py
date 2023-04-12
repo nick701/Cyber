695be65fe825864b4929d7690b7eb82f5ed0b87e
Nikolaos Tiliakos <Nikolaos.Tiliakos@loblaw.ca>
2023-04-12 11:18:45 -0400

    asdf

--- test2.py
+++
@@ -1,8 +0,0 @@
-def mult(num1, num2):
-    return num1 * num2
-
-# Random quote from People rarely succeed unless they have fun in what they are doing.: Bill Gates
-# Random quote: A man is a success if he gets up in the morning and gets to bed at night, and in: Anon.
-# Random quote:
- There are no secrets to success. It is the result of preparation, hard work, and: 
-Aisha Tyler
